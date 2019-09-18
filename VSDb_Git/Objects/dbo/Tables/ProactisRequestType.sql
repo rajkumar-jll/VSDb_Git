497abae043296129993f04fedd6d26170ca7a07e
rajkumar-jll <55374870+rajkumar-jll@users.noreply.github.com>
2019-09-18 14:55:01 +0530

    Update ProactisRequestType.sql

--- VSDb_Git/Objects/dbo/Tables/ProactisRequestType.sql
+++ VSDb_Git/Objects/dbo/Tables/ProactisRequestType.sql
@@ -1,7 +1,1 @@
-﻿CREATE TABLE [dbo].[ProactisRequestType] (
-    [Id]   INT           IDENTITY (1, 1) NOT NULL,
-    [NAME] NVARCHAR (50) NOT NULL,
-    CONSTRAINT [PK_ProactisRequestTypeId] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [AK_ProactisRequestType_Name] UNIQUE NONCLUSTERED ([NAME] ASC)
-);
 
